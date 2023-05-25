291b2efef75b3f246155860836a9b8575c9fd69e
Gabriel Maia <gribeiromaia736@gmail.com>
2023-05-25 12:02:54 -0300

    adicionando rotas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import http from 'http'
 //Rotas são o caminho de entrada na nossa API
 
+//Cabeçalho/Headers - São metadados que ajudam a ver como esse código pode ser interpretado pelo Front End
+
+const users =[]
+
 const server = http.createServer((req, res) => {
+  const { method, url }  = req
+
+  if(method === 'GET' && url === '/users') {
+    return res.end(JSON.stringify(users))
+  }
+  
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name:'Gabriel Maia',
+      idade:19
+    })
+    
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Helouu World')
 })
 
