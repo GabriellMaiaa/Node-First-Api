fc6e82ebe2512e0f70d6aff3ee7344468e1d1834
Gabriel Maia <gribeiromaia736@gmail.com>
2023-06-06 11:52:47 -0300

    adicionando função regex de buscar os ids

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,6 +2,10 @@
 //REGEX - É uma expressão regular
 
 export function buildRoutePath(path) {
-  const routeParmetersRegex = /:([a-zA-z]+)/g
-  console.log(Array.from(path.matchAll(routeParmetersRegex)))
+  const routeParmetersRegex = /:([a-zA-z]+)/g// Busca os que tem de A a z, mais o valor global
+  const pathWithParams = path.replaceAll(routeParmetersRegex, '(?<id>[a-z0-9\-_]+)')
+
+  const pathRegex = new RegExp(`${pathWithParams}`);
+
+  return pathRegex
 }
